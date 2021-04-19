06fb1466b94d1d9e1f9b19d56fce1ed88ed78f80
Saimon <47235269+SaimonK13@users.noreply.github.com>
2021-04-19 17:04:29 -0400

    Update IsUnique.java

--- src/IsUnique.java
+++ src/IsUnique.java
@@ -1,10 +1,10 @@
 public class IsUnique {
-	
 	//method 1: time complexity => O(n^2)
 	static String findUnique(String s) {
 		
 		char[] arr = s.toCharArray(); //parse the string into a character array
 		int len = arr.length;
+		
 		//compare the characters with each other
 		for (int i = 0; i < len; i++) {
 			for (int j = len-1; j > i; j--) {
@@ -25,13 +25,10 @@ public class IsUnique {
 		
 		//iterate over the string
 		for (int i = 0; i < s.length(); i++) {
-			
 			int charVal = s.charAt(i);//take integer value of character
-			
 			if(arr[charVal]) {
 				return "All characters are not unique"; //if the character already exists, return "not unique"
 			}
-			
 			else
 				arr[charVal] = true;
 		}
@@ -41,7 +38,6 @@ public class IsUnique {
 
 	//main method
 	public static void main(String[] args) {
-		
 		String str = "abcd";
 		System.out.println("Method 1: " + findUnique(str));
 		System.out.println("Method 2: " + findUnique2(str));
